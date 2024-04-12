a5e4b34a23412d41557a22d6cda2eeb5cb7c7c7a
Vinnie180 <markvincze2001@gmail.com>
2024-04-12 20:10:21 +0200

    Made ejs files and transfered the html code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
 require('./routes/outside')(app);
 
 app.use(express.static('static'));
