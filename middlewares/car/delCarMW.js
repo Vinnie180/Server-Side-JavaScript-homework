143e2b4a5d8145976f0c93eb3a1e831ca947900a
Vinnie180 <markvincze2001@gmail.com>
2024-05-16 13:47:05 +0200

    Made deleting cars

--- middlewares/car/delCarMW.js
+++ middlewares/car/delCarMW.js
@@ -6,6 +6,21 @@ const requireOption = require('../requireOption');
 
 module.exports = function (objectrepository) {
     return function (req, res, next) {
-        next();
+        const carModel = requireOption(objectrepository, 'carModel');
+
+        if (typeof res.locals.car === 'undefined') {
+            return next();
+        }
+
+        carModel.deleteOne({ _id: res.locals.car._id }).then(() => {
+            if (!car) {
+                return next(new Error('Car not found'));
+            }
+            return res.redirect('/cars');
+        }).catch(err => {
+            return next(err);
+        });
+
+        return next();
     };
 }
