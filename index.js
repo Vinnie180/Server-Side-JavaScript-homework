6902d3b272030136b29742a6a81892ef225d4ad9
Vinnie180 <markvincze2001@gmail.com>
2024-05-16 17:27:50 +0200

    Deleted comments made zip for handin

--- index.js
+++ index.js
@@ -21,37 +21,3 @@ app.use((err, req, res, next) => {
 app.listen(port, function () {
     console.log('Server is running on port 3000');
 });
-
-// //create a new car in the database
-
-// const carmodel = require('./models/car');
-// const car = new carmodel();
-// car.licensePlate = "ABC123";
-// car.brand = "Toyota";
-// car.length = 4.5;
-// car. capacity = 5;
-// car.trunkCapacity = 340;
-
-// //saving the car to mongodb database using mongoose
-
-// car.save().then(() => {
-//     console.log('Car has been saved');
-// }).catch((error) => {
-//     console.log('Error', error);
-// });
-
-// //create a new passenger in the database
-
-// const passengermodel = require('./models/passenger');
-// const passenger = new passengermodel();
-// passenger.name = "John Doe";
-// passenger.age = 30;
-// passenger.drivenHours = 100;
-// passenger._car = car;
-
-
-// passenger.save().then(() => {
-//     console.log('Passenger has been saved');
-// }).catch((error) => {
-//     console.log('Error', error);
-// });
