6ec55bbe69271e0b4610cb9ef857a46dec0b6264
Vinnie180 <markvincze2001@gmail.com>
2024-05-16 15:23:31 +0200

    Saving passengersMW done

--- middlewares/passenger/savePassengerMW.js
+++ middlewares/passenger/savePassengerMW.js
@@ -5,15 +5,28 @@
 const requireOption = require('../requireOption');
 
 module.exports = function (objectrepository) {
+
+    const passengerModel = requireOption(objectrepository, 'passengerModel');
+
     return function (req, res, next) {
-        
         if ((typeof req.body.name === 'undefined') ||
             (typeof req.body.age === 'undefined') ||
-            (typeof req.body.drivenHours === 'undefined')) {
+            (typeof req.body.drivenHours === 'undefined')||
+            (typeof res.locals.car === 'undefined')) {
             return next();
         }
-        console.log("savePassengerMW: " + req.body.name);
-        console.log(req.body);
-        res.redirect('/passengers/' + req.body._id);
+
+        if(typeof res.locals.passenger === 'undefined') {
+            res.locals.passenger = passengerModel();
+        }
+
+        res.locals.passenger.name = req.body.name;
+        res.locals.passenger.age = req.body.age;
+        res.locals.passenger.drivenHours = req.body.drivenHours;
+        res.locals.passenger._car = res.locals.car._id;
+
+        res.locals.passenger.save().then(passenger => {}).catch(err => { return next(err); });
+        
+        res.redirect('/passengers/' + res.locals.car._id);
     };
 }
