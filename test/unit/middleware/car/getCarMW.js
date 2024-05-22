060fca2d2b28a804bc6627f3cc07a0d37523eeeb
Vinnie180 <markvincze2001@gmail.com>
2024-05-22 17:46:38 +0200

    Reworked tests

--- test/unit/middleware/car/getCarMW.js
+++ test/unit/middleware/car/getCarMW.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var getCarMW = require('../../../../middlewares/car/getCarMW');
 
 describe('getCarMW middleware ', function () {
-    it('should return car', function (done) {
+    it('should return a car from the database by id with no error', function (done) {
         const mw = getCarMW({
             carModel: {
                 findOne: (p1) => {
@@ -16,11 +16,13 @@ describe('getCarMW middleware ', function () {
             locals: {}
         };
 
-        mw({
+        const reqMock = {
             params: {
                 carid: '1234'
             }
-        },
+        };
+
+        mw( reqMock,
             resMock,
             (err) => {
                 expect(err).to.not.exist;
@@ -29,24 +31,30 @@ describe('getCarMW middleware ', function () {
             });
     });
 
-    it('should call next with error', function (done) {
+    it('should call next with error if car not exist', function (done) {
         const mw = getCarMW({
             carModel: {
                 findOne: (p1) => {
                     expect(p1).to.be.eql({ _id: '1234' });
-                    return Promise.reject('error');
+                    return Promise.resolve(null);
                 }
             }
         });
 
-        mw({
+        const resMock = {
+            locals: {}
+        };
+
+        const reqMock = {
             params: {
                 carid: '1234'
             }
-        },
-            {},
+        };
+
+        mw( reqMock,
+            resMock,
             (err) => {
-                expect(err).to.be.eql('error');
+                expect(err).to.exist;
                 done();
             });
     });
